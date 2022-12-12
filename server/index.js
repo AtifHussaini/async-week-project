ced9960e1a086b5e399084dbeffa291166c87ee9
Atif Hussaini <ahussaini314@gmail.com>
2022-12-12 11:52:57 -0500

    Initial setup and created database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,7 @@ const port = process.env.PORT || 3000;
 const app = require('./app');
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+app.get("/", (req, res) => {
+    res.send("Hello :)")
+  })
